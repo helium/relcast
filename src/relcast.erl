3362aa2ba010db3ae5064255e81202c2834efe67
Rahul Garg <vihu89@gmail.com>
2018-09-27 14:54:15 -0700

    Remove hibarnate, continue from call/cast and change message type to binary

--- src/relcast.erl
+++ src/relcast.erl
@@ -10,23 +10,23 @@
 -callback restore(OldState :: term(), NewState :: term()) -> {ok, State :: term()}.
 -callback serialize(State :: term()) -> Binary :: binary().
 -callback deserialize(Binary :: binary()) -> State :: term().
--callback handle_message(Message :: any(), ActorId :: pos_integer(), State :: term()) ->
+-callback handle_message(Message :: binary(), ActorId :: pos_integer(), State :: term()) ->
     {NewState :: term(), Actions :: actions() | defer}.
 -callback handle_call(Request :: term(), From :: {pid(), Tag :: term()},
                      State :: term()) ->
    {reply, Reply :: term(), NewState :: term()} |
-   {reply, Reply :: term(), NewState :: term(), timeout() | hibernate | {continue, term()}} |
+   {reply, Reply :: term(), NewState :: term(), timeout()} |
    {noreply, NewState :: term()} |
-   {noreply, NewState :: term(), timeout() | hibernate | {continue, term()}} |
+   {noreply, NewState :: term(), timeout()} |
    {stop, Reason :: term(), Reply :: term(), NewState :: term()} |
    {stop, Reason :: term(), NewState :: term()}.
 -callback handle_cast(Request :: term(), State :: term()) ->
    {noreply, NewState :: term()} |
-   {noreply, NewState :: term(), timeout() | hibernate | {continue, term()}} |
+   {noreply, NewState :: term(), timeout()} |
 {stop, Reason :: term(), NewState :: term()}.
 
--type actions() :: [ {send, multicast, Message :: any()} |
-                     {send, unicast, To :: pos_integer(), Message :: any()} |
+-type actions() :: [ {send, multicast, Message :: binary()} |
+                     {send, unicast, To :: pos_integer(), Message :: binary()} |
                      {stop, Timeout :: timeout()} | new_epoch ].
 
 %%====================================================================
