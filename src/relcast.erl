2ff224726d5af7000381aa5c25b6dde87301e8c3
Andrew Thompson <andrew@hijacked.us>
2018-09-28 12:14:57 -0700

    Sketch out some ideas

--- src/relcast.erl
+++ src/relcast.erl
@@ -4,7 +4,7 @@
 -export([]).
 
 %%====================================================================
-%% API functions
+%% Callback functions
 %%====================================================================
 -callback init(Arguments :: any()) -> {ok, State :: term()}.
 -callback restore(OldState :: term(), NewState :: term()) -> {ok, State :: term()}.
@@ -28,7 +28,92 @@
 -type actions() :: [ {send, multicast, Message :: binary()} |
                      {send, unicast, To :: pos_integer(), Message :: binary()} |
                      {stop, Timeout :: timeout()} | new_epoch ].
+%%====================================================================
+%% Callback functions
+%%====================================================================
+
+-record(state, {
+          db,
+          module,
+          modulestate,
+          id,
+          ids
+          last_sent = #{},
+          pending_acks = #{}
+         }).
+
+-export([start/3, queue/3, take/2, ack/2]).
+
+start(ActorID, ActorIDs, Module, Arguments) ->
+    {ok, DB} = rocksdb:open(...),
+    case erlang:apply(Module, init, Arguments) of
+        {ok, ModuleState0} ->
+            ModuleState = case rocksdb:get(DB, <<"module_state">>) of
+                              {ok, SerializedModuleState} ->
+                                  {ok, OldModuleState} = Module:deserialize(SerializedModuleState),
+                                  {ok, RestoredModuleState} = Module:restore(OldModuleState, ModuleState0),
+                                  RestoredModuleState;
+                              {error, not_found} ->
+                                  ModuleState0
+                          end,
+            %% TODO try to deliver any old queued inbound messages
+            {ok, #state{module=Module, id=ActorID, ids=ActorIDs, modulestate=ModuleState, db=DB}};
+        _ ->
+            error
+    end.
+
+deliver(Message, FromActorID, State = #state{module=Module, modulestate=ModuleState, db=DB}) ->
+    Key = make_message_key(), %% some kind of predictable, monotonic key
+    rocksdb:store(DB, Key, <<FromActorID:16/integer, Message/binary>>),
+    case Module:handle_message(Message, FromActorID, ModuleState) of
+        defer ->
+            %% leave the message queued but we can ACK it
+            {ok, State};
+        {NewModuleState, Actions} ->
+            NewState = handle_actions(Actions, State),
+            rocksdb:store(DB, <<"module_state">>, Module:serialize(NewModuleState)),
+            {ok, NewState#state{modulestate=NewModuleState}}
+    end.
+
+
+take(ForActorID, State) ->
+    %% we need to find the first "unacked" message for this actor
+    %% we should remember the last acked message for this actor ID and start there
+    %% check if there's a pending ACK and use that to find the "last" key, if present
+    case maps:get(ForActorID, State#state.pending_acks, undefined) of
+        {Ref, Key} ->
+            {ok, Ref, rocksdb:get(DB, Key), State};
+        _ ->
+            Key = maps:get(ID, State#state.last_sent, <<0>>), %% default to the "first" key"
+            %% iterate until we find a key where the bit for this actor is not cleared
+            Iter = rocksdb:iterator(Key),
+            {Key, Msg} = find_next_message_for(ForActorID, Iter),
+            Ref = make_ref(),
+            {ok, Ref, Msg, State#state{pending_acks=maps:put(ForActorID, {Ref, Key}, State#state.pending_acks)}}
+    end.
+
+ack(FromActorID, Ref, State) ->
+    case maps:get(FromActorID, State#state.pending_acks, undefined) of
+        {Ref, Key} ->
+            NewPending = maps:delete(FromActorID, State#state.pending_acks),
+            {ok, State#state{pending_acks=NewPending, last_sent=maps:put(FromActorID, Key, State#state.last_sent}};
+        undefined ->
+            {ok, State}
+    end.
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
+
+handle_actions([], State) ->
+    State;
+handle_actions([{multicast, Message}|Tail], State) ->
+    Bitfield = make_bitfield(State#state.actor_ids),
+    rocksdb:put(DB, make_outbound_key(), <<BitField/binary, Message/binary>>),
+    handle_actions(Tail, State);
+handle_actions([{unicast, ToActorID, Message}|Tail], State) ->
+    Bitfield = make_bitfield(ToActorID),
+    rocksdb:put(DB, make_outbound_key(), <<BitField/binary, Message/binary>>),
+    handle_actions(Tail, State);
+handle_actions([{stop, Timeout}|Tail], State) ->
+    {stop, Timeout}.
