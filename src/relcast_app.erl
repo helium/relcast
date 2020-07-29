2eb245565e0ef5366e1aea9ab8ac433ca521ef31
Evan Vigil-McClanahan <evan@helium.com>
2020-07-29 13:46:30 -0700

    initial work

---
+++ src/relcast_app.erl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%% @doc relcast public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(relcast_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    relcast_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
